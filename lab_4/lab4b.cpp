23deb32b21c9f29620949cd61bebe9b63cea2575
Parz1web <71458569+Parz1web@users.noreply.github.com>
2025-05-30 22:10:05 +0300

    fixed lab4b

--- lab_4/lab4b.cpp
+++ lab_4/lab4b.cpp
@@ -4,43 +4,42 @@
 #include <fstream>
 #include <functional>
 #include <algorithm>
-using namespace std;
 
-auto sortAscending = [](string s1, string s2) {
+auto sortAscending = [](std::string s1, std::string s2) {
     return s1 < s2;
 };
 
 class BD {
 private:
-    vector<string> references_ru;
-    vector<string> references_en;
-    string fileName;
+    std::vector<std::string> references_ru;
+    std::vector<std::string> references_en;
+    std::string fileName;
 
-    vector<string> getSorted(vector<string> arr, function<bool(string, string)> sortFunction) {
+    std::vector<std::string> getSorted(std::vector<std::string> arr, std::function<bool(std::string, std::string)> sortFunction) {
         auto a = arr;
         sort(a.begin(), a.end(), sortFunction);
         return a;
     }
 
-    void display(vector<string> arr, bool withNumber) {
+    void display(std::vector<std::string> arr, bool withNumber) {
         for (int i = 0; i < arr.size(); i++) {
-            cout << (withNumber ? to_string(i + 1) + ".\t" : "") << arr[i] << endl;
+            std::cout << (withNumber ? std::to_string(i + 1) + ".\t" : "") << arr[i] << "\n";
         }
     }
 
 public:
-    BD(string fName) {
+    BD(const std::string& fName) {
         fileName = fName;
         loadFile();
     };
 
     void loadFile() {
-        ifstream file(fileName);
+        std::ifstream file(fileName);
         if (!file.is_open()) return;
         references_ru.clear();
         references_en.clear();
-        string line;
-        string number;
+        std::string line;
+        std::string number;
         while (getline(file, line)) {
             auto index = line.find(".") + 2;
             auto str = line.substr(index, line.size() - index);
@@ -49,8 +48,8 @@ public:
         }
     };
 
-    void display(function<bool(string, string)> sortFunction, bool withNumber) {
-        vector<string> commonArr = getSorted(references_ru, sortFunction);
+    void display(std::function<bool(std::string, std::string)> sortFunction, bool withNumber) {
+        std::vector<std::string> commonArr = getSorted(references_ru, sortFunction);
         for (auto el : getSorted(references_en, sortFunction)) commonArr.push_back(el);
         display(commonArr, withNumber);
     }
@@ -61,10 +60,10 @@ public:
 int main() {
     setlocale(LC_ALL, "Russian");
     BD db("data.txt");
-    cout << "References with numbers: \n";
+    std::cout << "References with numbers: \n";
     db.display(sortAscending, true);
 
-    cout << "\nReferences: \n";
+    std::cout << "\nReferences: \n";
     db.display(sortAscending, false);
     return 0;
 }
